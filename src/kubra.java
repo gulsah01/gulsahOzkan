64b0067ef9684aa7953c07206079c8c2006f4662
Gulsah Satilmis <136925048+gulsah01@users.noreply.github.com>
2023-10-13 18:23:29 +0100

    first commit

---
+++ src/kubra.java
@@ -0,0 +1,13 @@
+public class kubra {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Github");
+        /*
+        git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/gulsah01/gulsahOzkan.git
+git push -u origin main
+         */
+    }
+}
